5deb2cae6b67d9404d536ac27c67bed543462970
Mohamed Nabil <mohamed@email.com>
2026-02-15 19:00:43 +0200

    update3

--- utlits/generateVerificationToken.ts
+++ utlits/generateVerificationToken.ts
@@ -16,30 +16,7 @@ export const getVerificationToken = async(email:string) => {
 }
 
 export async function generateForgetPasswordToken(email:string) {
-    /*
-    const token = await prisma.forgetToken.findFirst({where:{email}});
-    if(!token) {
-        const forgetToken = await prisma.forgetToken.create({
-            data:{
-                email,
-                expires:new Date(Date.now() + 1000*60*60*2),
-                token:randomUUID()
-            }
-        })
-        return forgetToken
-    }
-    if(new Date(token.expires) < new Date()) {
-        await prisma.forgetToken.delete({where:{email}});
-        const forgetToken = await prisma.forgetToken.create({
-            data:{
-                email,
-                expires:new Date(Date.now() + 1000*60*60*2),
-                token:randomUUID()
-            }
-        })
-        return forgetToken
-    } 
-    return token;*/
+    
     await prisma.forgetToken.deleteMany({where:{email}})
     const forgetToken = await prisma.forgetToken.create({
         data:{
